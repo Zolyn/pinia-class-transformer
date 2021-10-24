c0ac6746ea161bcf2d4af9050cee9cc4fe2d1b1c
Zorin <zorin1477@gmail.com>
2021-10-24 19:00:03 +0800

    feat: change the way to get options of a store

--- src/options.ts
+++ src/options.ts
@@ -1,20 +1,23 @@
 import { DefineStoreOptions } from 'pinia';
-import { StatesAndGetters, Actions, Constructor, MetaDataKeys } from './types';
+import { StatesAndGetters, Actions, MetaDataKeys } from './types';
 
 /**
  * 获取目标类的Store选项
  *
+ * @param StoreId - Store的标识符
  * @param StoreClass - 用于编写Store的类
  *
  * @public
  */
-export function getOptions<I extends string, C>(
-    StoreClass: Constructor,
-): DefineStoreOptions<I, StatesAndGetters<C>, any, Actions<C>> {
-    return Reflect.getMetadata(MetaDataKeys.StoreOptions, StoreClass) as DefineStoreOptions<
-        I,
-        StatesAndGetters<C>,
-        any,
-        Actions<C>
-    >;
+export function getOptions<I extends string, C extends object>(StoreId: I, StoreClass: C) {
+    const storeOptionsWithoutId = Reflect.getMetadata(
+        MetaDataKeys.StoreOptions,
+        Object.getPrototypeOf(StoreClass).constructor,
+    );
+    const storeOptions: DefineStoreOptions<I, StatesAndGetters<C>, any, Actions<C>> = {
+        ...storeOptionsWithoutId,
+        id: StoreId,
+    };
+
+    return storeOptions;
 }
--- src/store.ts
+++ src/store.ts
@@ -4,43 +4,39 @@ import { StateTree, ActionsTree, GettersTree, DefineStoreOptions } from 'pinia';
 /**
  * Store装饰器工厂
  *
- * @param id - Store的标识符
- *
  * @public
  */
-export function Store(id: string): ClassDecorator {
-    return (StoreConstructor: Function) => {
-        // Avoid typesript warning
-        const ins = new StoreConstructor.prototype.constructor();
-        const states: StateTree = {};
-        Object.getOwnPropertyNames(ins).map((state): undefined => {
-            const descriptor = Object.getOwnPropertyDescriptor(ins, state);
-            if (descriptor) {
-                states[state] = descriptor.value;
-            }
+export function Store(StoreConstructor: Function): void {
+    // Avoid typesript warning
+    const ins = new StoreConstructor.prototype.constructor();
+    const states: StateTree = {};
+    Object.getOwnPropertyNames(ins).map((state): undefined => {
+        const descriptor = Object.getOwnPropertyDescriptor(ins, state);
+        if (descriptor) {
+            states[state] = descriptor.value;
+        }
 
-            return undefined;
-        });
+        return undefined;
+    });
 
-        const getters: GettersTree<StateTree> = {};
-        Object.getOwnPropertyNames(StoreConstructor.prototype).map((getter): undefined => {
-            const descriptor = Object.getOwnPropertyDescriptor(StoreConstructor.prototype, getter);
-            if (descriptor?.get) {
-                getters[getter] = descriptor.get;
-            }
+    const getters: GettersTree<StateTree> = {};
+    Object.getOwnPropertyNames(StoreConstructor.prototype).map((getter): undefined => {
+        const descriptor = Object.getOwnPropertyDescriptor(StoreConstructor.prototype, getter);
+        if (descriptor?.get) {
+            getters[getter] = descriptor.get;
+        }
 
-            return undefined;
-        });
+        return undefined;
+    });
 
-        const actions: ActionsTree = Reflect.getMetadata(MetaDataKeys.Actions, ins) ?? {};
+    const actions: ActionsTree = Reflect.getMetadata(MetaDataKeys.Actions, ins) ?? {};
 
-        const storeOptions: DefineStoreOptions<string, StateTree, GettersTree<StateTree>, ActionsTree> = {
-            id,
-            state: () => states,
-            getters,
-            actions,
-        };
-
-        Reflect.defineMetadata(MetaDataKeys.StoreOptions, storeOptions, StoreConstructor);
+    const storeOptions: DefineStoreOptions<string, StateTree, GettersTree<StateTree>, ActionsTree> = {
+        id: 'default id',
+        state: () => states,
+        getters,
+        actions,
     };
+
+    Reflect.defineMetadata(MetaDataKeys.StoreOptions, storeOptions, StoreConstructor);
 }
