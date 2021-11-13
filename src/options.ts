d746ffcca680f84b51982bd4b0fe85e71b763bc3
Zorin <zorin1477@gmail.com>
2021-11-13 22:28:00 +0800

    feat: simplified definition

--- src/options.ts
+++ src/options.ts
@@ -1,20 +1,18 @@
 import { DefineStoreOptions } from 'pinia';
-import { StatesAndGetters, Actions, MetaDataKeys } from './types';
+import { ValueOf } from 'type-fest';
+import { StatesAndGetters, Actions, Ctor, MetaDataKeys } from './types';
 
 /**
- * 根据给定的Store名称与类创建并返回定义Store的选项
+ * 根据给定的名称与类创建并返回定义Store的选项
  *
  * @param StoreId - Store的名称
- * @param StoreClass - 编写Store的类
+ * @param StoreConstructor - 编写Store的类（构造函数）
  *
  * @public
  */
-export function createOptions<I extends string, C extends object>(StoreId: I, StoreClass: C) {
-    const storeOptionsBase = Reflect.getMetadata(
-        MetaDataKeys.StoreOptions,
-        Object.getPrototypeOf(StoreClass).constructor,
-    );
-    const storeOptions: DefineStoreOptions<I, StatesAndGetters<C>, any, Actions<C>> = {
+export function createOptions<I extends string, C extends Ctor>(StoreId: I, StoreConstructor: C) {
+    const storeOptionsBase = Reflect.getMetadata(MetaDataKeys.StoreOptions, StoreConstructor);
+    const storeOptions: DefineStoreOptions<I, StatesAndGetters<ValueOf<C>>, any, Actions<ValueOf<C>>> = {
         ...storeOptionsBase,
         id: StoreId,
     };
