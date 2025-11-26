8877ea856c4942327c7c2d90a80cb1109e5760db
Zolyn <zorin1477@gmail.com>
2025-11-26 16:59:33 +0800

    test: add setup call test

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,78 +1,97 @@
-import { computed, watchEffect } from 'vue'
-import { beforeEach, expect, test, describe, vi } from 'vitest'
-import { createPinia, getActivePinia, Pinia, PiniaPlugin, setActivePinia, StoreGeneric } from 'pinia'
-import { defineOptionStore, defineSetupStore, useContext } from '../src'
-import type { SetupStore } from '../src/setupStore'
-import type { OptionStore } from '../src/optionStore'
-import type { Class } from 'type-fest'
-import { StateTree } from '../src/types/shared'
+import { computed, watchEffect } from "vue";
+import { beforeEach, expect, test, describe, vi } from "vitest";
+import {
+  createPinia,
+  getActivePinia,
+  Pinia,
+  PiniaPlugin,
+  setActivePinia,
+  StoreGeneric,
+} from "pinia";
+import { defineOptionStore, defineSetupStore, useContext } from "../src";
+import type { SetupStore } from "../src/setupStore";
+import type { OptionStore } from "../src/optionStore";
+import type { Class } from "type-fest";
+import { StateTree } from "../src/types/shared";
 
-beforeEach(() => setActivePinia(createPinia()))
+beforeEach(() => setActivePinia(createPinia()));
 
 type TestingPinia = Pinia & {
   _p: PiniaPlugin[];
-  _s: Map<string, StoreGeneric>
-}
+  _s: Map<string, StoreGeneric>;
+};
 
 function getActiveTestingPinia(): TestingPinia {
   const pinia = getActivePinia() as TestingPinia | undefined;
 
   if (!pinia) {
-    throw new Error('No active pinia found');
+    throw new Error("No active pinia found");
   }
 
   pinia.use = function (plugin: PiniaPlugin) {
     this._p.push(plugin);
-    return this
-  }
+    return this;
+  };
 
-  return pinia
+  return pinia;
 }
 
-type SetupStoreFn = <S extends object>(storeClass: Class<S>) => SetupStore<S>
-type OptionStoreFn = <S extends object>(storeClass: Class<S>) => OptionStore<S>
+type SetupStoreFn = <S extends object>(storeClass: Class<S>) => SetupStore<S>;
+type OptionStoreFn = <S extends object>(storeClass: Class<S>) => OptionStore<S>;
 
-function createSharedTest(testFn: (defineFn: SetupStoreFn | OptionStoreFn) => void) {
-  return () => test.each([
-    ['Option', defineOptionStore],
-    ['Setup', defineSetupStore]
-  ])('%s', (_, defineFn) => {
-    testFn(defineFn)
+function createSharedTest(
+  testFn: (defineFn: SetupStoreFn | OptionStoreFn) => void
+) {
+  return () =>
+    test.each([
+      ["Option", defineOptionStore],
+      ["Setup", defineSetupStore],
+    ])("%s", (_, defineFn) => {
+      testFn(defineFn);
+    });
+}
+
+function runSharedTest(
+  testFn: (defineFn: SetupStoreFn | OptionStoreFn) => void
+) {
+  createSharedTest(testFn)();
+}
+
+describe(
+  "Simple store",
+  createSharedTest((defineFn) => {
+    class Store {
+      count = 1;
+      get double() {
+        return this.count * 2;
+      }
+      inc() {
+        this.count++;
+      }
+    }
+    const useStore = defineFn(Store);
+    const store = useStore();
+
+    expect(store.count).toBe(1);
+    expect(store.double).toBe(2);
+    store.inc();
+    expect(store.count).toBe(2);
+    expect(store.double).toBe(4);
   })
-}
+);
 
-function runSharedTest(testFn: (defineFn: SetupStoreFn | OptionStoreFn) => void) {
-  createSharedTest(testFn)()
-}
-
-describe('Simple store', createSharedTest((defineFn) => {
-  class Store {
-    count = 1;
-    get double() { return this.count * 2 }
-    inc() { this.count++ }
-  }
-  const useStore = defineFn(Store);
-  const store = useStore();
-
-  expect(store.count).toBe(1);
-  expect(store.double).toBe(2);
-  store.inc();
-  expect(store.count).toBe(2);
-  expect(store.double).toBe(4);
-}))
-
-describe('Inheritance', () => {
+describe("Inheritance", () => {
   runSharedTest((defineFn) => {
     class Base {
       baseCount = 1;
       baseAction() {
-        this.baseCount++
+        this.baseCount++;
       }
     }
     class Store extends Base {
       childCount = 2;
       childAction() {
-        this.childCount++
+        this.childCount++;
       }
     }
 
@@ -87,55 +106,65 @@ describe('Inheritance', () => {
     store.childAction();
     expect(store.baseCount).toBe(2);
     expect(store.childCount).toBe(3);
-  })
+  });
 
-  describe('Override', createSharedTest((defineFn) => {
-    class Base {
-      baseCount = 1;
-      baseAction() {
-        this.baseCount++
+  describe(
+    "Override",
+    createSharedTest((defineFn) => {
+      class Base {
+        baseCount = 1;
+        baseAction() {
+          this.baseCount++;
+        }
       }
-    }
-    class Store extends Base {
-      baseCount = 2;
-      baseAction() {
-        this.baseCount *= 2
+      class Store extends Base {
+        baseCount = 2;
+        baseAction() {
+          this.baseCount *= 2;
+        }
+      }
+
+      const useStore = defineFn(Store);
+      const store = useStore();
+
+      expect(store.baseCount).toBe(2);
+      store.baseAction();
+      expect(store.baseCount).toBe(4);
+    })
+  );
+});
+
+describe(
+  "Setup function with no return value",
+  createSharedTest((defineFn) => {
+    const fn = vi.fn();
+
+    class Store {
+      count = 1;
+      setup() {
+        watchEffect(() => fn(this.count), { flush: "sync" });
       }
     }
 
     const useStore = defineFn(Store);
     const store = useStore();
 
-    expect(store.baseCount).toBe(2);
-    store.baseAction();
-    expect(store.baseCount).toBe(4);
-  }))
-})
+    expect(fn).toHaveBeenCalledTimes(1);
+    store.count++;
+    expect(fn).toHaveBeenCalledTimes(2);
+  })
+);
 
-describe('Setup function with no return value', createSharedTest((defineFn) => {
-  const fn = vi.fn();
-
-  class Store {
-    count = 1;
-    setup() {
-      watchEffect(() => fn(this.count), { flush: 'sync' })
-    }
-  }
-
-  const useStore = defineFn(Store);
-  const store = useStore();
-
-  expect(fn).toHaveBeenCalledTimes(1);
-  store.count++;
-  expect(fn).toHaveBeenCalledTimes(2);
-}))
-
-describe('Setup Store', () => {
-  test('Writable computed', () => {
+describe("Setup Store", () => {
+  test("Writable computed", () => {
     class Store {
       count = 1;
-      get double() { return this.count * 2 }
-      set double(v) { this.count = v / 2 }
+      get double() {
+        return this.count * 2;
+      }
+      set double(v) {
+        this.count = v / 2;
+      }
     }
 
     const useStore = defineSetupStore(Store);
@@ -145,15 +174,15 @@ describe('Setup Store', () => {
     store.double = 10;
     expect(store.count).toBe(5);
     expect(store.double).toBe(10);
-  })
+  });
 
-  test('Setup return merge', () => {
+  test("Setup return merge", () => {
     class Store {
       count = 1;
       setup() {
-        const double = computed(() => this.count * 2)
-        const inc = () => this.count++
-        return { double, inc }
+        const double = computed(() => this.count * 2);
+        const inc = () => this.count++;
+        return { double, inc };
       }
     }
 
@@ -164,16 +193,18 @@ describe('Setup Store', () => {
     store.inc();
     expect(store.count).toBe(2);
     expect(store.double).toBe(4);
-  })
+  });
 
-  test('Setup return override', () => {
+  test("Setup return override", () => {
     class Store {
       count = 1;
-      get double() { return 'double' }
+      get double() {
+        return "double";
+      }
 
       setup() {
-        const double = computed(() => this.count * 2)
-        return { double }
+        const double = computed(() => this.count * 2);
+        return { double };
       }
     }
 
@@ -181,39 +212,42 @@ describe('Setup Store', () => {
     const store = useStore();
 
     expect(store.double).toBe(2);
-  })
-})
+  });
+});
 
-describe('Pinia property accessing', () => {
-  describe('Common properties', createSharedTest((defineFn) => {
-    class Store {
-      count = 1;
-      get f(): number {
-        return useContext<Store>(this).$state.count
+describe("Pinia property accessing", () => {
+  describe(
+    "Common properties",
+    createSharedTest((defineFn) => {
+      class Store {
+        count = 1;
+        get f(): number {
+          return useContext<Store>(this).$state.count;
+        }
+        increasetByPatching() {
+          useContext<Store>(this).$patch({ count: this.count + 1 });
+        }
       }
-      increasetByPatching() {
-        useContext<Store>(this).$patch({ count: this.count + 1 })
-      }
-    }
 
-    type A = StateTree<Store>
+      type A = StateTree<Store>;
 
-    const useStore = defineFn(Store);
-    const store = useStore();
+      const useStore = defineFn(Store);
+      const store = useStore();
 
-    expect(store.f).toBe(1);
-    store.increasetByPatching();
-    expect(store.f).toBe(2);
-  }))
+      expect(store.f).toBe(1);
+      store.increasetByPatching();
+      expect(store.f).toBe(2);
+    })
+  );
 
-  test('$reset (Option only)', () => {
+  test("$reset (Option only)", () => {
     class Store {
       count = 1;
       increment() {
-        this.count++
+        this.count++;
       }
       reset() {
-        useContext(this).$reset()
+        useContext(this).$reset();
       }
     }
 
@@ -225,21 +259,45 @@ describe('Pinia property accessing', () => {
     expect(store.count).toBe(2);
     store.reset();
     expect(store.count).toBe(1);
-  })
+  });
 
-  describe('Custom property', createSharedTest((defineFn) => {
+  describe(
+    "Custom property",
+    createSharedTest((defineFn) => {
+      class Store {
+        count = 1;
+        get a() {
+          return (this as any).$test;
+        }
+      }
+
+      const pinia = getActiveTestingPinia();
+      pinia.use(() => ({ $test: "test" }));
+
+      const useStore = defineFn(Store);
+      const store = useStore();
+      expect(store.a).toBe("test");
+    })
+  );
+});
+
+describe(
+  "Setup should be called once",
+  createSharedTest((defineFn) => {
+    const fn = vi.fn();
+
     class Store {
       count = 1;
-      get a() {
-        return (this as any).$test
+      setup() {
+        console.log(this.count);
+        fn();
       }
     }
 
-    const pinia = getActiveTestingPinia();
-    pinia.use(() => ({ $test: 'test' }))
-
-    const useStore = defineFn(Store);
-    const store = useStore();
-    expect(store.a).toBe('test');
-  }))
-})
+    const useStore = defineOptionStore(Store);
+    const _storeA = useStore();
+    expect(fn).toHaveBeenCalledTimes(1);
+    const _storeB = useStore();
+    expect(fn).toHaveBeenCalledTimes(1);
+  })
+);
